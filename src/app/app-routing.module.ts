390cc0e72ac92ce4d8d50dfce7ee339f72a2c1b5
villasana26enrique <enrique.villasana26@gmail.com>
2020-06-06 18:21:07 -0400

    Instalando FA. Animate.css, Creando componentes y Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HeroesListComponent } from './pages/heroes-list/heroes-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesListComponent      },
+  { path: 'heroe/:id', component: HeroeComponent        },
+  { path: '**', pathMatch: 'full', redirectTo: 'heroes' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
