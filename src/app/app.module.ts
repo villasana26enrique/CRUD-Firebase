c7dc522eb5b6467a9644c8b7f508b2e3a6f7ae7e
villasana26enrique <enrique.villasana26@gmail.com>
2020-06-06 21:48:30 -0400

    Diseño de Vistas y creando Formulario Reactivo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HeroesListComponent } from './pages/heroes-list/heroes-list.component';
@@ -14,7 +15,8 @@ import { HeroesListComponent } from './pages/heroes-list/heroes-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-heroe',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.createForm();
   }
+
+  private createForm() {
+    this.form = this.fb.group({
+      firebase_id : [ { value: '', disabled: true } ],
+      name  : [ '', [ Validators.required] ],
+      power : [ '' ],
+    });
+  }
+
+  save() {
+    console.log(this.form);
+  }
+
+  ngOnInit(): void {}
 
 }
