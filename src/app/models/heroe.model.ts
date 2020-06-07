7c3cc9467735ad2f28619cc46c7df97c20a39a88
villasana26enrique <enrique.villasana26@gmail.com>
2020-06-06 22:54:30 -0400

    Recibiendo información del form

---
+++ src/app/models/heroe.model.ts
@@ -0,0 +1,12 @@
+export class HeroeModel {
+
+    id: string;
+    name: string;
+    power: string;
+    alive: boolean;
+
+    constructor() {
+        this.alive = true;
+    }
+
+}
--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HeroeModel } from '../../models/heroe.model';
 
 @Component({
   selector: 'app-heroe',
@@ -8,22 +9,43 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class HeroeComponent implements OnInit {
 
+  heroe = new HeroeModel();
   form: FormGroup;
 
   constructor(private fb: FormBuilder) {
     this.createForm();
+    this.loadForm();
   }
 
   private createForm() {
     this.form = this.fb.group({
-      firebase_id : [ { value: '', disabled: true } ],
+      id    : [ { value: '', disabled: true } ],
       name  : [ '', [ Validators.required] ],
-      power : [ '' ],
+      power : [ '' ]
+    });
+  }
+
+  loadForm() {
+    this.form.reset({
+      id: this.heroe.id,
+      name: this.heroe.name,
+      power: this.heroe.power
     });
   }
 
   save() {
-    console.log(this.form);
+
+    if ( this.form.invalid ) {
+      console.log('Formulario no válido');
+      return;
+    }
+
+    let heroeRequest: HeroeModel;
+    heroeRequest = {
+      ...this.form.value,
+      ...this.heroe
+    };
+    console.log(heroeRequest);
   }
 
   ngOnInit(): void {}
